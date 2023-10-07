0505259adc90ae26b6a6dec32a3f59983a8db7a8
Adebozz <ademolaadeigbe3@gmail.com>
2023-10-07 14:30:09 +0100

    third commit

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ const sequilize = require('./util/database');
 const User = require('./models/user');
 
 const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-control-allow-Origin', '*');
+    res.setHeader('Access-control-allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
+//test route
+app.get('/', (req, res, next) => {
+    res.send('Hello World');
+});
+
+//error handling
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
+
+//sync database
+sequilize
+    .sync()
+    .then(result => {
+        console.log("Database connected");
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
