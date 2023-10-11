90fd2bc6089b344baf5adf5f7c7b5ce8ee8ed9a8
Adebozz <ademolaadeigbe3@gmail.com>
2023-10-11 23:35:02 +0100

    update

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const User = db.define("users", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
-    allowNull: true,
+    allowNull: false,
     primaryKey: true,
   },
   name: Sequelize.STRING,
